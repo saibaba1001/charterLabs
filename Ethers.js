d10d1ea9ce270586de138cbec318f3be4e1e4ee1
saibaba <143478017+saibaba1001@users.noreply.github.com>
2024-11-09 16:34:30 +0530

    Ethers.js
    ///I don't know why i couldn't able to fetch the blocknumber i mean a transaction from Sepolia or zkSync it's showing like ("to many request as well as time outed")///

--- Ethers.js
+++ Ethers.js
@@ -6,7 +6,7 @@ const provider = new ethers.JsonRpcProvider("YOUR_INFURA_OR_ALCHEMY_ENDPOINT");
 ///I don't know why i couldn't able to fetch the blocknumber i mean a transaction from Sepolia or zkSync it's showing like ("to many request as well as time outed")///
 
 async function fetchTransactions(blockNumber) {
-    const block = await provider.getBlockWithTransactions(blockNumber);
+    const block = await provider.getBlockWithTransactions(blockNumber); 
     const transactions = block.transactions.map(tx => tx.hash);
     return transactions;
 }
